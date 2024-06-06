6e3a81a8173f1b80eef8ad8ca71c4230b4aed250
kurtn3x <kurtn3x@proton.me>
2024-06-06 16:31:22 +0200

    -

---
+++ roles/dns/files/gen_blocklist.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "server:" >/etc/unbound//unbound.conf.d/blacklist.conf
+curl -s https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts | \
+        grep ^0.0.0.0 - | \
+        sed 's/ #.*$//;
+        s/^0.0.0.0 \(.*\)/local-zone: "\1" refuse/' \
+        >>/etc/unbound/unbound.conf.d/blacklist.conf
